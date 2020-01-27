ad2be3760c275c5a950838067eb14d9c313a8df7
zzq2 <zzq2@meitu.com>
2020-01-27 13:43:49 +0800

    fix

--- LeetCode/005_longest_palindrome/longest_palindrome.py
+++ LeetCode/005_longest_palindrome/longest_palindrome.py
@@ -1,54 +1,43 @@
+class Solution(object):
+    def check_palindrome(self, s, i, j):
+        length = 0
+        while True:
+            i = i - 1
+            j = j + 1
+            if i < 0 or j > len(s) - 1:
+                break
+            if s[i] == s[j]:
+                length += 1
+            else:
+                break
 
-def check_palindrome(s, i, j):
-    length = 0
-    while True:
-        i = i - 1
-        j = j + 1
-        if i < 0 or j > len(s)-1:
-            return length
-        if s[i] == s[j]:
-            length += 1
-        else:
-            return length
+        return length
 
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if len(s) <= 1:
+            return s
 
-
-def longestPalindrome(s):
-    """
-    :type s: str
-    :rtype: str
-    """
-    length = len(s)
-    sublen = 0
-
-    if length <= 1:
-        return s
-    
-    max_length = 0
-    maxstrfinal = ''
-    for i in range(length-1):
-        begin = i
-        end = i
-        sublen = check_palindrome(s, begin, end)
-        maxstr = s[begin-sublen:end+sublen+1]
-        if s[i] == s[i+1]:
+        maxstrfinal = ''
+        maxlength = 0
+        for i in range(len(s) - 1):
             begin = i
-            end = i + 1
-            sublen2 = check_palindrome(s, begin, end)
-            if sublen2 >=  sublen:
-                maxstr = s[begin-sublen2:end+sublen2+1]
+            end = i
+            sublen = self.check_palindrome(s, begin, end)
+            maxstr = s[begin-sublen:end+sublen+1]
 
-        if len(maxstr) > max_length:
-            max_length = len(maxstr)
-            maxstrfinal = maxstr
+            if s[i] == s[i+1]:
+                begin = i
+                end = i + 1
+                sublen2 = self.check_palindrome(s, begin, end)
+                if sublen2 >= sublen:
+                    maxstr = s[begin-sublen2:end+sublen2+1]
 
-    return maxstrfinal
+            if len(maxstr) > maxlength:
+                maxstrfinal = maxstr
+                maxlength = len(maxstr)
 
-                    
-
-if __name__ == "__main__":
-    s = "ac"
-    res = check_palindrome(s, 3, 3)
-    respalindrome = longestPalindrome(s)
-    print(res)
-    print(respalindrome)
+        return maxstrfinal
