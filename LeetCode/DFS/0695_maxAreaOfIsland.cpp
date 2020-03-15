4cdcba75a857d629a69aa6396a3d1e98cdd4c4ab
zzq2 <zzq2@meitu.com>
2020-03-15 12:54:48 +0800

    leetcode 0315

---
+++ LeetCode/DFS/0695_maxAreaOfIsland.cpp
@@ -0,0 +1,58 @@
+/*
+给定一个包含了一些 0 和 1的非空二维数组 grid , 一个 岛屿 是由四个方向 (水平或垂直) 的 1 (代表土地) 构成的组合。你可以假设二维矩阵的四个边缘都被水包围着。
+
+找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为0。)
+
+示例 1:
+
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+对于上面这个给定矩阵应返回 6。注意答案不应该是11，因为岛屿只能包含水平或垂直的四个方向的‘1’。
+
+示例 2:
+
+[[0,0,0,0,0,0,0,0]]
+对于上面这个给定的矩阵, 返回 0。
+
+注意: 给定的矩阵grid 的长度和宽度都不超过 50。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/max-area-of-island
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution
+{
+public:
+    int dfs(vector<vector<int>> &grid, int cur_i, int cur_j)
+    {
+        if (cur_i < 0 || cur_j < 0 || cur_i == grid.size() || cur_j == grid[0].size() || grid[cur_i][cur_j] != 1)
+            return 0;
+        grid[cur_i][cur_j] = 0;
+        int di[4] = {0, 0, 1, -1};
+        int dj[4] = {1, -1, 0, 0};
+        int ans = 1;
+        for (int index = 0; index != 4; ++index)
+        {
+            int next_i = cur_i + di[index], next_j = cur_j + dj[index];
+            ans += dfs(grid, next_i, next_j);
+        }
+        return ans;
+    }
+
+public:
+    int maxAreaOfIsland(vector<vector<int>> &grid)
+    {
+        int ans = 0;
+        for (int i = 0; i != grid.size(); ++i)
+            for (int j = 0; j != grid[0].size(); ++j)
+                ans = max(ans, dfs(grid, i, j));
+        return ans;
+    }
+};
